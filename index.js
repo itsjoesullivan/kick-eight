f275b4f6b81de424047381312e80ba836fc76ab7
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-28 13:15:15 -0500

    tweak timing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function(context) {
   var osc = context.createOscillator();
+  osc.frequency.value = 55;
   var gain = context.createGain();
   osc.connect(gain);
 
@@ -8,9 +9,10 @@ module.exports = function(context) {
       when = context.currentTime;
     }
     osc.start(when);
-    gain.setValueAtTime(0.0001, when);
-    gain.exponentialRampToValueAtTime(1, when + 0.01);
-    gain.exponentialRampToValueAtTime(0.0001, when + 1);
+    gain.gain.setValueAtTime(0.0001, when);
+    gain.gain.exponentialRampToValueAtTime(1, when + 0.005);
+    gain.gain.exponentialRampToValueAtTime(0.5, when + 0.1);
+    gain.gain.exponentialRampToValueAtTime(0.0001, when + 1.5);
   };
 
   gain.stop = function(when) {
