6a0ac1e70120dcb7795d93c9bd9dd4e8ae3d9710
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-28 13:33:57 -0500

    build

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/Users/joe/dev/kicky/index.js":[function(require,module,exports){
 module.exports = function(context) {
   var osc = context.createOscillator();
-  osc.frequency.value = 55;
+  osc.frequency.value = 54;
   var gain = context.createGain();
   osc.connect(gain);
 
@@ -12,8 +12,7 @@ module.exports = function(context) {
     osc.start(when);
     gain.gain.setValueAtTime(0.0001, when);
     gain.gain.exponentialRampToValueAtTime(1, when + 0.005);
-    gain.gain.exponentialRampToValueAtTime(0.5, when + 0.1);
-    gain.gain.exponentialRampToValueAtTime(0.0001, when + 1.5);
+    gain.gain.exponentialRampToValueAtTime(0.0001, when + .3);
   };
 
   gain.stop = function(when) {
