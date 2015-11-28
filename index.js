fee899bc87cfcc31839d1d1f3874cad4aeb98ee4
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-28 13:34:23 -0500

    update timing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function(context) {
   var osc = context.createOscillator();
-  osc.frequency.value = 55;
+  osc.frequency.value = 54;
   var gain = context.createGain();
   osc.connect(gain);
 
@@ -11,8 +11,7 @@ module.exports = function(context) {
     osc.start(when);
     gain.gain.setValueAtTime(0.0001, when);
     gain.gain.exponentialRampToValueAtTime(1, when + 0.005);
-    gain.gain.exponentialRampToValueAtTime(0.5, when + 0.1);
-    gain.gain.exponentialRampToValueAtTime(0.0001, when + 1.5);
+    gain.gain.exponentialRampToValueAtTime(0.0001, when + .3);
   };
 
   gain.stop = function(when) {
